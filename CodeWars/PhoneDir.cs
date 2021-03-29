8e9aa9a23e112a692bb54c7ed002d80cc67583b0
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-03-29 09:50:31 -0600

    Add project files.

---
+++ CodeWars/PhoneDir.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class PhoneDir
+    {
+        public static string Phone(string strng, string num)
+        {
+            // your code
+
+            return "";
+        }
+    }
+}
---
+++ Isomorphic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Isomorphic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
