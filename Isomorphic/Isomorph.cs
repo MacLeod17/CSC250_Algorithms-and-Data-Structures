08af3a03b2987c6b50704f1667b3ee0aa80d257b
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-01 09:49:14 -0600

    Added basic setup for Isomorph

---
+++ Isomorphic/Isomorph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Isomorphic
+{
+    public class Isomorph
+    {
+        public static void GenerateIsomorphs(string path)
+        {
+            IEnumerable<string> result = File.ReadLines(path);
+
+            // "0 0 1", List<string>
+            Dictionary<string, List<string>> exactIsoList = new Dictionary<string, List<string>>();
+
+            exactIsoList.Add("0 0 1", new List<string> { "egg" });
+            foreach (var item in exactIsoList)
+            {
+                string key = item.Key;
+                List<string> value = item.Value;
+
+                var words = String.Join(" ", value);
+            }
+
+            List<string> funWords = new List<string> { "Dog", "cat", "frog", "duck" };
+            Console.WriteLine(String.Join(", ", funWords));
+        }
+
+        public static void ExactIsomorph(string word)
+        {
+            
+        }
+    }
+}
--- Isomorphic/Program.cs
+++ Isomorphic/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Isomorphic
 {
-    class Program
+    public class Program
     {
+        #region My Paths
+        // C:\Users\Garrick Kilpack\source\repos\Algorithms & Data Structures\ALGO\Isomorphic\TextFiles\IsomorphInput1.txt
+        // C:\Users\Garrick Kilpack\source\repos\Algorithms & Data Structures\ALGO\Isomorphic\TextFiles\IsomorphInput2.txt
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Path to File?");
+            var path = Console.ReadLine();
+            Isomorph.GenerateIsomorphs(path);
         }
     }
 }
