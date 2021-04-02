76a779e3b9a34e9b352b78e75f2cbb2b285397fd
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-02 12:31:13 -0600

    Started Grid CodeWars

---
+++ CodeWars/Grid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class Grid
+    {
+        public static int NumberOfRectangles(int w, int h)
+        {
+            // Your code here!
+            return 0;
+        }
+    }
+}
---
+++ TestingProject/GridTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWars
+{
+    [TestFixture]
+    public class GridTests
+    {
+        [Test]
+        public void Test1()
+        {
+            int rectangles = Grid.NumberOfRectangles(4, 4);
+            Assert.AreEqual(100, rectangles);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int rectangles = Grid.NumberOfRectangles(5, 5);
+            Assert.AreEqual(225, rectangles);
+        }
+    }
+}
