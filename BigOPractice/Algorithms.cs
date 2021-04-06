b637e8acd3059db8125f2867345954012ca7fed0
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-05 19:58:24 -0600

    Started BigOPractice bruh

---
+++ BigOPractice/Algorithms.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigOPractice
+{
+    public class Algorithms
+    {
+        public static List<int> MatchMe(List<int> a1, List<int> a2)
+        {
+            List<int> matchingNumbers = new List<int>();
+            int nestedIndex = 0;
+            int operationCount = 0;
+
+            for (int i = 0; i < a1.Count; i++)
+            {
+                for (int j = 0; j < a2.Count; j++)
+                {
+                    operationCount++;
+                    //If first item in a1 equal to item in second (a2) list
+                    if (a2[i] == a2[j])
+                    {
+                        // Add matching number
+                        matchingNumbers.Add(a1[i]);
+                        nestedIndex = j + 1;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Operations: " + operationCount);
+            return matchingNumbers;
+        }
+        public static int Algorithim(int[] A)
+        {
+            int operationCount = 0;
+            if (A.Length > 0)
+            {
+                // Last item in the array
+                int x = A.Length - 1;
+
+                //first item in the Array / largest Number
+                int y = A[0];
+
+                //for i q to x d
+                //Is current index value greater than stored
+                for (int i = 1; i <= x; i++)
+                {
+                    operationCount++;
+                    if (A[i] > y)
+                    {
+                        //y = a[i]
+                        //We found a new high value, set it!
+                        y = A[i];
+                    }
+                }
+                Console.WriteLine("Operations: " + operationCount);
+
+                // return the largest number
+                return y;
+            }
+            return -1;
+        }
+    }
+}
---
+++ BigOPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigOPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            Random random = new Random();
+
+            // long[] useMe = gen
+            List<int> match1 = GenerateArray(250000).ToList();
+            List<int> match2 = GenerateArray(250000).ToList();
+
+            match1.Sort();
+            match2.Sort();
+            //watch.Start();
+            //var highestValue = Algorithims.Algorithim(useMe);
+            //Algorithims.Algorithim(useMe);
+
+            watch.Start();
+            var matchingArray = Algorithms.MatchMe(match1, match2);
+            watch.Stop();
+
+            Console.WriteLine("[" + String.Join(", ", matchingArray) + "]");
+
+            //Console.WriteLine("Highest Value: " + highestValue);
+
+            var elapsedTime = watch.ElapsedMilliseconds != 0 ? watch.ElapsedMilliseconds : watch.ElapsedTicks;
+            string milliOrTricks = watch.ElapsedMilliseconds == 0 ? "Ticks" : "ms";
+            Console.WriteLine($"Execution Time {elapsedTime} " + milliOrTricks);
+        }
+
+        public static int[] GenerateArray(int numberOfItems)
+        {
+            int[] returnArray = new int[numberOfItems];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                returnArray[i] = rand.Next(0, numberOfItems);
+            }
+            return returnArray;
+        }
+    }
+}
