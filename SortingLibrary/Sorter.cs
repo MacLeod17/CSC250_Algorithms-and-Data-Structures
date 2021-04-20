175a88e82a05e765c93421980c08de2e9b481d6d
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-20 11:39:39 -0600

    MergeSort and QuickSort now work

--- SortingLibrary/Sorter.cs
+++ SortingLibrary/Sorter.cs
@@ -67,12 +67,104 @@ namespace SortingLibrary
 
         public static void QuickSort(T[] arr)
         {
-            
+            QuickSortHelper(arr, 0, arr.Length-1);
+        }
+
+        static void QuickSortHelper(T[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                T pivot = arr[high];
+                int i = low - 1;
+
+                for (int j = low; j <= high - 1; j++)
+                {
+                    if (arr[j].CompareTo(pivot) <= 0)
+                    {
+                        i++;
+
+                        var temp1 = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp1;
+                    }
+                }
+
+                var temp2 = arr[i + 1];
+                arr[i + 1] = arr[high];
+                arr[high] = temp2;
+
+                int p = i + 1;
+
+                QuickSortHelper(arr, low, p - 1);
+                QuickSortHelper(arr, p + 1, high);
+            }
         }
 
         public static void MergeSort(T[] arr)
         {
 
+            MergeSortHelper(arr, 0, arr.Length-1);
+        }
+
+        static void MergeSortHelper(T[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+
+                MergeSortHelper(arr, l, m);
+                MergeSortHelper(arr, m + 1, r);
+
+                int len1 = m - l + 1;
+                int len2 = r - m;
+
+                T[] tempLeft = new T[len1];
+                T[] tempRight = new T[len2];
+                int i;
+                int j;
+
+                for (i = 0; i < len1; ++i)
+                {
+                    tempLeft[i] = arr[l + i];
+                }
+                for (j = 0; j < len2; ++j)
+                {
+                    tempRight[j] = arr[m + 1 + j];
+                }
+
+                i = 0;
+                j = 0;
+                int k = l;
+
+                while (i < len1 && j < len2)
+                {
+                    if (tempLeft[i].CompareTo(tempRight[j]) <= 0)
+                    {
+                        arr[k] = tempLeft[i];
+                        i++;
+                    }
+                    else
+                    {
+                        arr[k] = tempRight[j];
+                        j++;
+                    }
+                    k++;
+                }
+
+                while (i < len1)
+                {
+                    arr[k] = tempLeft[i];
+                    i++;
+                    k++;
+                }
+
+                while (j < len2)
+                {
+                    arr[k] = tempRight[j];
+                    j++;
+                    k++;
+                }
+            }
         }
     }
 }
