04c04e90d9c19b7432fb14a43aa8ebeafff087bb
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-20 08:05:06 -0600

    Flood Fill algorithm works, but not efficient enough

--- CodeWars/Kata.cs
+++ CodeWars/Kata.cs
@@ -6,6 +6,25 @@ namespace CodeWars
 {
     public class Kata
     {
+        public static int[,] FloodFill(int[,] array, int y, int x, int newValue, int oldValue = int.MinValue)
+        {
+            if (oldValue == int.MinValue) oldValue = array[y, x];
+
+            int lenY = array.GetLength(0) - 1;
+            int lenX = array.GetLength(1) - 1;
+
+            if (array[y, x] != oldValue || array[y, x] == newValue) return array;
+
+            array[y, x] = newValue;
+
+            if (y < lenY) array = FloodFill(array, y + 1, x, newValue, oldValue);
+            if (y > 0) array = FloodFill(array, y - 1, x, newValue, oldValue);
+            if (x < lenX) array = FloodFill(array, y, x + 1, newValue, oldValue);
+            if (x > 0) array = FloodFill(array, y, x - 1, newValue, oldValue);
+
+            return array;
+        }
+
         public static bool IsValidWalk(string[] walk)
         {
             int n = 0;
@@ -43,5 +62,6 @@ namespace CodeWars
 
             return false;
         }
+        
     }
 }
---
+++ TestingProject/FloodFillTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    [TestFixture]
+    public class FloodFillTests
+    {
+        [Test]
+        public void TestExample()
+        {
+            var expected = new int[,]
+            { 
+                { 1, 4, 3 },
+                { 1, 4, 4 },
+                { 2, 3, 4 } 
+            };
+
+            var actual = new int[,]
+            {
+                { 1, 2, 3 },
+                { 1, 2, 2 },
+                { 2, 3, 2 }
+            };
+
+            CollectionAssert.AreEqual(expected, Kata.FloodFill(actual, 0, 1, 4));
+        }
+    }
+}
