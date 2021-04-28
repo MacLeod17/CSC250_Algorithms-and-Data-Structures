0eb61778014fcd59bd6e73756bfc39c477c6a0e5
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-28 11:25:43 -0600

    Queens algorithm start

--- NQueenLab/NQueens.cs
+++ NQueenLab/NQueens.cs
@@ -6,16 +6,60 @@ namespace NQueenLab
 {
     public static class NQueens
     {
-        // store solutions at class level
+        // Store Solutions at Class Level
+        public static List<List<int>> allSolutions = new List<List<int>>();
+        public static int N;
+        public static int steps;
+
         public static void SolveQueens(int n)
         {
-            FindNQueens(n, 0);
+            N = n;
+
+            FindNQueens(0, new List<int>());
         }
 
-        public static void FindNQueens(int n, int row)
+        public static void FindNQueens(int row, List<int> solution)
         {
+            if (row >= N)
+            {
+                // ?
+                solution.Add(steps);
+                allSolutions.Add(new List<int>(solution));
+                solution.RemoveAt(solution.Count - 1);
+            }
 
-            // iterate columns
+            // Iterate Columns
+            else
+            {
+                for (int col = 0; col < N; col++)
+                {
+                    if (IsSafe(solution, col, row))
+                    {
+                        solution.Add(col);
+                        steps++;
+                        FindNQueens(row + 1, solution);
+                        // Remove Queen?
+                        solution.RemoveAt(solution.Count - 1);
+                    }
+                }
+            }
+        }
+
+        static bool IsSafe(List<int> solution, int col, int row)
+        {
+            for (int i=0; i < row; i++)
+            {
+                // Check Vertical
+                if (solution[i] == col) return false;
+
+                // Check Diagonal Up-Left
+                //if (solution[i] == -MathF.Abs(col - i)) return false;
+
+                // Check Diagonal Up-Right
+                if (solution[i] == MathF.Abs(col - i)) return false;
+            }
+            
+            return true;
         }
     }
 }
